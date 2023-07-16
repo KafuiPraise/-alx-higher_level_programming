522708b4cac030a46cedf8efd1a2ffd6484d77ec
kafuipraise <matildapraise@gmail.com>
2023-07-16 22:52:44 +0000

    fixing syntax  errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         my_list.reverse()
-        for b in range (len(my_list)):
-            print("{:d}".format(my_list[b]))
-         
+        for b in range(len(my_list)):
+            print("{:d}".format(my_list[b])) 
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-     """
-    a function that prints a matrix of integers
-    """
-     for r in matrix:
-          print(" ".join("{:d}".format(c) for c in r))
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
