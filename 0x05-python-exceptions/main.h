2152eec1c7bbb38e6bb221a18d7266500a6b7af2
KafuiPraise <matildapraise@gmail.com>
2023-08-01 02:29:31 +0000

    fixing bugs

---
+++ 0x05-python-exceptions/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+#include <python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
