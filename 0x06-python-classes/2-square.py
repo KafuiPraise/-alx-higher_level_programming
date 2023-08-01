974c64402310ae9f6c57c583a224e626fdc995fd
KafuiPraise <address>
2023-08-01 15:28:38 -0700

    Mandatory Task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""defines a class Square."""
+"""Define the class Square."""
 
 
 class Square:
-    """Will represent Square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-    Args:
-        size (int): The size of the new square.
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0
-    raise ValueError("size must be >= 0")
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
