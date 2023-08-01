f5ae89e384091c598cbaf399b410de7d40fbe0ac
vagrant <vagrant@vagrant.vm>
2023-08-01 21:19:23 +0000

    mandatory task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+#Creating a blue print for Square
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#1-square.py
+"""Defining a class Square."""
+
+class Square:
+    """Represents a Sqaure"""
+
+
+    def __init__(self, size):
+        """initialize a new Square.
+        args:
+            size (int): The size of a new square.
+        """
+        
+        self.__size = size
