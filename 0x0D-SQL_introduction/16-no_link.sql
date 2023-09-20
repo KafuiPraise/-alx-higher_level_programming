a0e7b25deff3a6054ee2215e861f7e529d4cc053
KafuiPraise <address>
2023-09-19 23:46:55 -0700

    All Task

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
--- ists all records of the table second_table of the database 
-SELECT 'score', ' name'
-FROM 'second_table'
-WHERE 'name' IS NOT NULL
-ORDER BY 'score' DESC;
+-- ists all records of the table second_table of the database
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
