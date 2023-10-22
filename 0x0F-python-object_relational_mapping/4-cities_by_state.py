f09d40a532c13499c028c08ea2bbffa68b86337e
kafuipraise <matildapraise@gmail.com>
2023-10-22 14:52:38 -0700

    Mandatory Task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""  lists all states from the database """
 
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306, charset="utf8")
+    db = MySQLdb.connect(
+                        host="localhost",
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306, 
+                        charset="utf8"
+                        )
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
-    for r in row:
+    for r in rows:
         print(r)
     cur.close()
     db.close()
