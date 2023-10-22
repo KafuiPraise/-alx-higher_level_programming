1a10a412c5060ee31104b64a37d5f630350a836e
kafuipraise <matildapraise@gmail.com>
2023-10-22 12:37:12 -0700

    All Tasks

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" List states in module """
+
+import MySQLdb
+import sys
+
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    cur = conn.cursor()
+    query = "SELECT id,name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for r in row:
+        print(r)
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()i
