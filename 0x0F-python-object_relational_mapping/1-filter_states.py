af0a0b87091d8e14ef6ca2fe46d6b91b413be7ce
kafuipraise <matildapraise@gmail.com>
2023-10-22 13:35:04 -0700

    Mandatory Task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""All states listed from the database hbtn_0e_0_usa """
+""" Module list state where name start with N """
 
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306), charset="utf8"
+    db = MySQLdb.connect(
+                        host="localhost", 
+                        user=sys.argv[1], 
+                        passwd=sys.argv[2], 
+                        db=sys.argv[3], 
+                        port=3306), 
+                        charset="utf8"
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    for r in rows:
+        print(r)
     cur.close()
     db.close()
