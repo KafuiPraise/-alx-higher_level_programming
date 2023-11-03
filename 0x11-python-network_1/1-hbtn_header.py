c04a262c9cbdd8ddaacf99d9636e5055f952cf04
KafuiPraise <matildapraise@gmail.com>
2023-11-03 08:39:57 -0700

    Mandatory task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
+"""A script that:recieves in a URL,
+- sends  request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response.
 """
 
 import sys
-import urllib.request
+from urllib.request import urlopen
+from urllib.parse import urlencode
 
-if __name__ == "__main__":
-    # Check if the user provided a URL as a command-line argument
-    if len(sys.argv) < 2:
-        print("Please provide a URL.")
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 3:
+        print('Usage: ', __file__, 'URL', 'email', file=sys.stderr)
         sys.exit(1)
 
-    # Get the URL from the command-line arguments
-    url = sys.argv[1]
+    data = urlencode({'email': sys.argv[2]}).encode()
 
-    # Send a request to the specified URL
-    try:
-        with urllib.request.urlopen(url) as response:
-            # Retrieve and print the value of the "X-Request-Id" header
-            x_request_id = response.headers.get("X-Request-Id")
-            print(f"X-Request-Id: {x_request_id}")
-    except urllib.error.URLError as e:
-        print(f"Error: {e}")
-
+    with urlopen(sys.argv[1], data=data) as r:
+        print(r.read().decode())
