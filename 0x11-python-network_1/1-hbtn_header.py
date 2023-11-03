8f3fb12af447717a9f6e04998a9fdc212ea1d93b
KafuiPraise <matildapraise@gmail.com>
2023-11-03 08:43:17 -0700

    Mandatory task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
+
 """A script that:recieves in a URL,
 - sends  request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response.
 """
 
 import sys
-from urllib.request import urlopen
-from urllib.parse import urlencode
+import urllib.request
 
 
-if __name__ == '__main__':
-
-    if len(sys.argv) != 3:
-        print('Usage: ', __file__, 'URL', 'email', file=sys.stderr)
-        sys.exit(1)
-
-    data = urlencode({'email': sys.argv[2]}).encode()
-
-    with urlopen(sys.argv[1], data=data) as r:
-        print(r.read().decode())
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as output:
+        req = output.info()
+        print(req.get('X-Request-Id'))
